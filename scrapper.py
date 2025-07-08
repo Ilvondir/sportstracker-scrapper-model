a1e195b4e20e22fd1187bea5e3acaad64ad4960d
Ilvondir <komsa.m@o2.pl>
2025-07-08 23:31:04 +0200

    load workouts

--- scrapper.py
+++ scrapper.py
@@ -1,5 +1,4 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from dotenv import load_dotenv
 import os
 import time
@@ -10,6 +9,7 @@ email = os.getenv('SPORTSTRACKER_EMAIL')
 password = os.getenv('SPORTSTRACKER_PASSWORD')
 
 driver = uc.Chrome()
+driver.implicitly_wait(2)
 
 # Login
 driver.get("https://www.sports-tracker.com/login")
@@ -21,9 +21,7 @@ print(f'\t3. Click ACCEPT ALL at cookiees baner.')
 print(f'\t4. Click LOGIN.')
 print(f'\t5. Wait for script.')
 
-time.sleep(30)
-
-driver.implicitly_wait(2)
+time.sleep(15)
 
 if False:
     email_field = driver.find_element(by=By.CSS_SELECTOR, value='input[type="text"].username')
@@ -39,6 +37,24 @@ if False:
     driver.find_element(by=By.CSS_SELECTOR, value='input[type="submit"].submit').click()
 
 
+# Load all workouts
+driver.find_element(by=By.CSS_SELECTOR, value='a[href="/diary"]').click()
+time.sleep(1)
+driver.find_elements(by=By.CLASS_NAME, value='tab')[1].click()
+time.sleep(1)
 
+while True:
+    elem = driver.find_element(by=By.CLASS_NAME, value='show-more')
+
+    if 'ng-hide' in elem.get_attribute('class'):
+        break
+
+    elem.click()
+    time.sleep(0.5)
 
 driver.close()
+
+all_elems = driver.find_elements(by=By.CSS_SELECTOR, value='.diary-list__workout a')
+identificators = [elem.get_attribute('href')[elem.get_attribute('href').rfind('/')+1:] for elem in all_elems]
+
+# Download all GPX files
